9301cbcbf9ee10fe9eb3e4f2bc51c430dc8584b0
Jesper Jakobsen <jesper.jakobsen@alexandra.dk>
2022-08-29 15:52:05 +0200

    Make elements request cancelable

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -4,29 +4,27 @@ const ndjsonStream = require("can-ndjson-stream");
 
 export const useElements = (): UseQueryResult<Row[], unknown> => {
   const queryClient = useQueryClient();
-  return useQuery<Row[]>(["elements"], async () => {
+  return useQuery<Row[]>(["elements"], async ({ signal }) => {
     const response = await fetch("http://localhost:8080", {
       headers: {
         accept: "application/x-ndjson",
       },
+      signal: signal,
     });
-    const ndjson = ndjsonStream(response.body)
-    const reader = ndjson.getReader()
+    const ndjson = ndjsonStream(response.body);
+    const reader = ndjson.getReader();
 
     while (true) {
-      const { done, value } = await reader.read()
+      const { done, value } = await reader.read();
       if (done) {
         break;
       }
-
-      // Update state
       queryClient.setQueryData(["elements"], (oldData: Row[] | undefined) => {
         return Array.isArray(oldData) ? [...oldData, value] : [value];
       });
     }
     reader.releaseLock();
-    const data = queryClient.getQueryData<Row[]>(["elements"])
-
+    const data = queryClient.getQueryData<Row[]>(["elements"]);
     return Array.isArray(data) ? data : [];
   });
 };
